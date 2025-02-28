663218e9f7fa5c574deca591add3a13f3de499e6
YiWei <94545667+wyy-py@users.noreply.github.com>
2025-02-28 10:21:51 +0800

    Update database.py

--- database.py
+++ database.py
@@ -3,3 +3,4 @@ from config import config
 client = MongoClient(config.MONGO_URI)
 db = client[config.DB]
 collection = db[config.COLLECTION]
+users_collection = db[config.USER_COLLECTION]  # 存储用户信息的 Collection
